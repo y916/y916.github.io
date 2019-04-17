8dffbd0f65857df7092ebdbd8595d2e7f82a51a8
Littor <yangfang9714@gmail.com>
2019-04-17 12:28:06 +0800

    Update writer.py

--- DATA/test/writer.py
+++ DATA/test/writer.py
@@ -7,6 +7,6 @@ aac = session.xyDataObjects.items()
 for i in range(len(aac)):
     x0 = session.xyDataObjects[aac[i][0]]
     session.writeXYReport(fileName=MDS[i] + '.rpt', appendMode=OFF, xyData=(x0, ))
-    del x0
+    del session.xyDataObjects[aac[i][0]]
 
 session.odbs[ONM].close()
